50846eb8ff62a1f85fba2ce711e0b934c7176c99
tom <ruotaohe@163.com>
2016-06-10 17:43:03 +0800

    commit linemod_pose_estimation package and its dependencies. missing readme.md file

---
+++ birl_vision/linemod/cv_bridge3/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tom/software/vision_opencv/cv_bridge3/build/devel:/home/tom/ros/indigo/objectRecog_ws/devel:/home/tom/ros/indigo/class_ws/devel:/home/tom/baxter_ws/devel:/home/tom/catkin_ws/devel:/opt/ros/indigo"
+export CPATH="/home/tom/ros/indigo/class_ws/devel/include:/home/tom/baxter_ws/devel/include:/home/tom/catkin_ws/devel/include:/opt/ros/indigo/include:/usr/local/include"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu:/home/tom/ppf_ws/devel/lib:/home/tom/ur_ws/devel/lib:/home/tom/tutorial_ws/devel/lib:/home/tom/industrial_training/training/work/2.5b/devel/lib:/home/tom/industrial_training/training/supplements/devel/lib:/home/tom/ork_ws/devel/lib:/home/tom/ros/indigo/objectRecog_ws/devel/lib:/home/tom/ros/indigo/class_ws/devel/lib:/home/tom/baxter_ws/devel/lib:/home/tom/catkin_ws/devel/lib:/opt/ros/indigo/lib:/usr/lib:/usr/local/lib:/etc/alternatives/"
+export PATH="/home/tom/baxter_ws/devel/bin:/opt/ros/indigo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/tom/ppf_ws/devel/lib/pkgconfig:/home/tom/ur_ws/devel/lib/pkgconfig:/home/tom/tutorial_ws/devel/lib/pkgconfig:/home/tom/industrial_training/training/work/2.5b/devel/lib/pkgconfig:/home/tom/industrial_training/training/supplements/devel/lib/pkgconfig:/home/tom/ork_ws/devel/lib/pkgconfig:/home/tom/ros/indigo/objectRecog_ws/devel/lib/pkgconfig:/home/tom/ros/indigo/class_ws/devel/lib/pkgconfig:/home/tom/baxter_ws/devel/lib/pkgconfig:/home/tom/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/tom/ros/indigo/class_ws/devel/lib/python2.7/dist-packages:/home/tom/baxter_ws/devel/lib/python2.7/dist-packages:/home/tom/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tom/software/vision_opencv/cv_bridge3/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/tom/software/vision_opencv/cv_bridge3:$ROS_PACKAGE_PATH"
